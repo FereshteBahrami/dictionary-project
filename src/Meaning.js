1ce32633abe699ad056eca479c18a1116dbd8f82
fereshte <fereshte.bahramy@gmail.com>
2022-04-25 13:19:05 +0430

    Added phonetics

--- src/Meaning.js
+++ src/Meaning.js
@@ -2,7 +2,6 @@ import React from "react";
 import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
-  console.log(props.meaning);
   return (
     <div className="Meaning ">
       <h3>{props.meaning.partOfSpeech}</h3>
@@ -13,7 +12,7 @@ export default function Meaning(props) {
               <strong>Definition:</strong> {definition.definition}
               <br />
               <strong>Example:</strong>
-              <em>{definition.example}</em>
+              <em> {definition.example}</em>
               <br />
               <Synonyms synonyms={definition.synonyms} />
             </p>
