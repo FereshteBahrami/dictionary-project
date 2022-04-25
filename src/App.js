e014ff53eeceab6a2b31e4dd0086a150a780fd04
fereshte <fereshte.bahramy@gmail.com>
2022-04-25 20:24:31 +0430

    Styled the application

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from "./logo.jpg";
 import Dictionary from "./Dictionary";
 import "./App.css";
 
@@ -7,10 +6,8 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
-        <Dictionary />
+        <header className="App-header">Dictionary</header>
+        <Dictionary defaultKeyword="sunset" />
         <footer className="App-footer">
           <small>Coded by Fereshte Bahrami</small>
         </footer>
