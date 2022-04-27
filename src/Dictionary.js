a9face80b90a47f7d2eeb897e9c4119adbbd4eb6
fereshte <fereshte.bahramy@gmail.com>
2022-04-28 02:46:08 +0430

    Added photo API integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,22 +1,33 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data[0]);
     setResults(response.data[0]);
+  }
+
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   function search() {
     //documentation: https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+
+    let pexelsApiKey =
+      "563492ad6f9170000100000156e6d7987a984b278fe52821825f8f25 ";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=6`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
   function handleSubmit(event) {
@@ -39,21 +50,22 @@ export default function Dictionary(props) {
       <div className="Dictionary  ">
         <section>
           <h1>What do you want to look up?</h1>
-          <div class="form">
-            <form onSubmit={handleSubmit}>
-              <input
-                type="text"
-                placeholder="search"
-                onChange={handleKeywordChange}
-                defaultValue={props.defaultKeyword}
-              />
-            </form>
-          </div>
+
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder="search"
+              onChange={handleKeywordChange}
+              defaultValue={props.defaultKeyword}
+            />
+          </form>
+
           <div className="hint">
             suggested word: sunset, wine, yoga, plant...
           </div>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
